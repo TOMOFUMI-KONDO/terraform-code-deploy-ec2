59a2d03d1f134569acd1ffa70eefbcf2da230f64
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-06 17:44:34 +0900

    added profile to script arg

--- script/create_deployment.sh
+++ script/create_deployment.sh
@@ -3,10 +3,12 @@
 PROJECT=$1
 REPOSITORY=$2
 COMMIT=$3
+PROFILE=$4
 
 aws deploy create-deployment \
     --application-name "$PROJECT"-code-deploy-app \
     --deployment-config-name CodeDeployDefault.OneAtATime \
     --deployment-group-name "$PROJECT"-code-deploy-group \
     --description "deployment for terraform sandbox" \
-    --github-location repository="$REPOSITORY",commitId="$COMMIT"
+    --github-location repository="$REPOSITORY",commitId="$COMMIT" \
+    --profile "$PROFILE"
--- script/setup_code_deploy_agent.sh
+++ script/setup_code_deploy_agent.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 INSTANCE_ID=$1
+PROFILE=$2
 
 aws ssm send-command \
     --document-name "AWS-ConfigureAWSPackage" \
     --instance-ids "$INSTANCE_ID" \
     --parameters '{"action":["Install"],"installationType":["Uninstall and reinstall"],"name":["AWSCodeDeployAgent"]}' \
+    --profile "$PROFILE"
